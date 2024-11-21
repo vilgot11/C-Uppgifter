4d9f5f7c5e38134bd9a5a1655470293464180309
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-21 11:41:11 +0200

    Add files via upload

---
+++ C#Uppgift1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vilgot";
+            Console.WriteLine(name);
+
+            int num = 5;
+            Console.WriteLine(num);
+            int num2 = 10;
+            Console.WriteLine(num2);
+
+            
+            
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
