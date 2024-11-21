21e8fd80604dfbe7dcc691e8cd9fc1719b1dcec2
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-21 11:41:46 +0200

    Add files via upload

---
+++ C#Uppgift3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + "!");
+
+           Console.ReadLine();
+        }
+    }
+}
