887b736f2c1c6f7b6321ba6b865dfc27c1ddc747
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-21 11:42:34 +0200

    Add files via upload

---
+++ C#Uppgift5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a gread: ");
+            int gread = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (gread)
+            {
+                case 5:
+                    Console.WriteLine("Excellent");
+                    break;
+                case 4:
+                    Console.WriteLine("Good");
+                    break;
+                case 3:
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 2:
+                    Console.WriteLine("ok");
+                    break;
+                case 1:
+                    Console.WriteLine("Needs Improvement");
+                    break;
+                case 0:
+                    Console.WriteLine("Fail");
+                    break;
+            }
+            Console.ReadLine();
+            }
+    }
+}
