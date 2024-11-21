ed23285cc5b6b20d84a47ac9082a5a96c0be27c8
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-21 11:41:31 +0200

    Add files via upload

---
+++ C#Uppgift2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "3";
+            int number = int.Parse(myString);
+            Console.WriteLine(myString);
+
+
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
