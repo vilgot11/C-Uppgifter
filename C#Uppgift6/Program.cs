c3745490d9c575cb6917ec1fdb50a3d9cfa3e8a3
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-21 11:42:51 +0200

    Add files via upload

---
+++ C#Uppgift6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Value of loops? ");
+            string message = Console.ReadLine();
+
+            Console.Write("How many repeats?");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+
+
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine(message);
+                }
+
+            Console.WriteLine("loop done");
+
+                Console.ReadLine();
+
+
+            }
+    }
+}
