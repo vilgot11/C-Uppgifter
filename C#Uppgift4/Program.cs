be59484e702ddfb54a66a23d0625a9044f987095
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-21 11:42:07 +0200

    Add files via upload

---
+++ C#Uppgift4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int age = 17;
+
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Eligible to vote!: ");
+            }
+                else
+            {
+                Console.WriteLine("Not Eligible to vote: ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
