653c29e2588387827f35bb327835ee1485a8652a
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-25 14:03:23 +0200

    Add files via upload

---
+++ C#Uppgift10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            int num4 = 4;
+            int num5 = 5;
+            int num6 = 6;
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num3 * num4);
+            Console.WriteLine(num5 / num6);
+
+            Console.WriteLine(++num6);
+            Console.WriteLine(--num6);
+
+            Console.WriteLine(num5 += 5);
+            Console.WriteLine(num5 -= 5);
+            Console.WriteLine(num5 *= 5);
+            Console.WriteLine(num5 /= 5);
+            Console.WriteLine(num5 %= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Uppgift10/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("C#Uppgift10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C#Uppgift10")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("535aca03-c1c0-4e08-bb5f-2a4d0ce1cd11")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
