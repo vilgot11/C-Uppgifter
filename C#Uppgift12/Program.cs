3a28d2ef7d463de600e4cb89ad79a247e05b427a
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-25 14:03:59 +0200

    Add files via upload

---
+++ C#Uppgift12/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift12
+{
+    internal class Program
+    {
+        private static string password2;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("write password");
+            string password1 = Console.ReadLine();
+
+            Console.WriteLine("write password again");
+            string password2 = Console.ReadLine();
+
+
+
+
+            if (password1.Length <= 5)
+            {
+                Console.WriteLine("Write pleas write longer password");
+            }
+
+            if (password2.Length <= 5)
+            {
+                Console.WriteLine("Write pleas write longer password");
+            }
+
+
+            if (password1 == null || password1 == "")
+            {
+                Console.WriteLine("pleas enter a Password");
+            }
+
+            if (password2 == null || password2 == "")
+            {
+                Console.WriteLine("pleas enter a Password");
+            }
+
+
+
+
+
+
+            if (password1 == password2)
+            {
+                Console.WriteLine("Password match");
+            }
+            else
+            {
+                Console.WriteLine("Password do not match");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
