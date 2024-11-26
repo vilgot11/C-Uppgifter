f511ce9dcc7232916019478569909f407f52b073
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-26 13:34:13 +0200

    Add files via upload

---
+++ C#Uppgift13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Number is even: " + number);
+            }
+            else
+            {
+                Console.WriteLine("Number is odd: " + number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
