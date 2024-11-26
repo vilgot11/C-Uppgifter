dbd6fcd00384a6ea40fc06cf196990a1d84a8737
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-26 14:04:10 +0200

    Add files via upload

---
+++ C#Uppgift16/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 45, 12, 78, 34, 89, 23, };
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+
+
+            Array.Sort(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Array.Reverse(numbers);
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+
+
+
+            int numToRemove = 78;
+            int numIndex = Array.IndexOf(numbers, numToRemove);
+            numbers = numbers.Where((val, idx) => idx != numIndex).ToArray();
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+
+            int index = Array.IndexOf(numbers, 34); 
+            Console.WriteLine(index); 
+
+
+
+            Console.ReadLine();
+
+        }
+        }
+}
