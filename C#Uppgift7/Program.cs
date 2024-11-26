bf03b18419be856a682c88ca8d85a099c54216b9
vilgot11 <vilgot.ahlfors@gmail.com>
2024-11-26 13:33:51 +0200

    Add files via upload

---
+++ C#Uppgift7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int Counter = 1;
+            while (Counter <= 5)
+            {
+                Console.WriteLine(Counter + 1);
+                Counter++;
+                
+            }
+
+            Console.WriteLine("loop Finished");
+            Console.ReadLine();
+
+
+
+        } 
+           
+        }
+    }
+
