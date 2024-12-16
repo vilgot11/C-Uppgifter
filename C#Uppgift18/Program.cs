7bf4b1b54a140095820c3fa97a49725c802088ac
vilgot11 <vilgot.ahlfors@gmail.com>
2024-12-16 08:56:23 +0200

    Add files via upload

---
+++ C#Uppgift18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift18
+{
+    internal class Program
+    {
+
+
+        
+        static void Main(string[] args)
+        {
+           
+        
+        
+            Console.WriteLine("Enter Widght: ");
+            double Widght = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            double Height = Convert.ToDouble(Console.ReadLine());
+
+            double area = (Widght * Height) / 2;
+
+            Console.WriteLine($"The area of the triangle is {area}");
+
+            Console.ReadLine();
+        }
+        
+    }
+        
+        
+    }
+
